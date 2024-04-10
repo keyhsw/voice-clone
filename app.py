11fdcf53c575f5ea68d59397f2ccbb256ff92ffe
Tony Assi <tonyassi@users.noreply.huggingface.co>
2024-04-10 16:10:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import gradio as gr
+import torch
+from TTS.api import TTS
+import os
+os.environ["COQUI_TOS_AGREED"] = "1"
+
+device = "cuda"
+
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 def clone(text, audio):
-    return audio
+    tts.tts_to_file(text=text, speaker_wav=audio, language="en", file_path="./output.wav")
+    return "./output.wav"
 
 iface = gr.Interface(fn=clone, 
                      inputs=["text",gr.Audio(type='filepath')], 
