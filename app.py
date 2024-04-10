c79aed5a51027f397eabc841322c0fa298d6c38e
Tony Assi <tonyassi@users.noreply.huggingface.co>
2024-04-10 16:01:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ def clone(text, audio):
     return audio
 
 iface = gr.Interface(fn=clone, 
-                     inputs=["text","audio"], 
-                     outputs="audio")
+                     inputs=["text",gr.Audio(type='filepath')], 
+                     outputs=gr.Audio(type='filepath'))
 iface.launch()
